3446ddbaf95ee0fdf86c48e7d72e380424091697
Nicholas Warfield <nick@nkwarfield.io>
2022-07-15 13:49:55 -0700

    Remove color from vertex

--- include/vertex.hpp
+++ include/vertex.hpp
@@ -10,7 +10,6 @@ namespace gfx {
 
 struct Vertex {
 	glm::vec3 position;
-	glm::vec3 color;
 	glm::vec2 tex_coord;
 
 	static VkVertexInputBindingDescription get_binding_description() {
@@ -22,8 +21,8 @@ struct Vertex {
         return binding_description;
     }
 
-	static std::array<VkVertexInputAttributeDescription, 3> get_attribute_description() {
-		std::array<VkVertexInputAttributeDescription, 3> attribute_descriptions {};
+	static std::array<VkVertexInputAttributeDescription, 2> get_attribute_description() {
+		std::array<VkVertexInputAttributeDescription, 2> attribute_descriptions {};
 
 		attribute_descriptions[0].binding = 0;
 		attribute_descriptions[0].location = 0;
@@ -32,13 +31,8 @@ struct Vertex {
 
 		attribute_descriptions[1].binding = 0;
 		attribute_descriptions[1].location = 1;
-		attribute_descriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-		attribute_descriptions[1].offset = offsetof(Vertex, color);
-
-		attribute_descriptions[2].binding = 0;
-		attribute_descriptions[2].location = 2;
-		attribute_descriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-		attribute_descriptions[2].offset = offsetof(Vertex, tex_coord);
+		attribute_descriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
+		attribute_descriptions[1].offset = offsetof(Vertex, tex_coord);
 
 		return attribute_descriptions;
 	}
